90bb1659ca5a18c69de5dc881f640e9f677ac90c
Bakhyt Sembayev <bahytsembaev@gmail.com>
2024-04-05 19:49:13 +0500

    2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useForm } from 'react-hook-form';
+import styles from './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const sendFormData = (formData) => {
+    console.log(formData);
+};
+
+export const App = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm({
+        defaultValues: {
+            login: '',
+        },
+    });
+
+    const loginProps = {
+        minLength: { value: 3, message: 'Неверный логин. Должно быть не меньше 3 символов' },
+        maxLength: { value: 20, message: 'Неверный логин. Должно быть не больше 20 символов' },
+        pattern: {
+            value: /^[w_]*$/,
+            message: 'Неверный логин. Допустимые символы: буквы, цифры и нижнее подчёркивание',
+        },
+    };
+
+    const loginError = errors.login?.message;
+
+    return (
+        <div className={styles.app}>
+            <form onSubmit={handleSubmit(sendFormData)}>
+                {loginError && <div className={styles.errorLabel}>{loginError}</div>}
+                <input name="login" type="text" {...register('login', loginProps)} />
+                <button type="submit" disabled={!!loginError}>Отправить</button>
+            </form>
+        </div>
+    );
+};
 
 export default App;
